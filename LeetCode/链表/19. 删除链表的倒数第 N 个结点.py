2272c826a1829adf638487ab658285b8a7367bac
ElcarimQAQ <1067095308@qq.com>
2024-11-18 13:39:46 +0800

    update 删除链表

---
+++ LeetCode/链表/19. 删除链表的倒数第 N 个结点.py
@@ -0,0 +1,23 @@
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(next=head)
+        right = dummy
+        for _ in range(n):
+            right = right.next
+        
+        left = dummy
+        while right.next:
+            left = left.next
+            right = right.next
+        left.next = left.next.next
+        return dummy.next
+            
+        
---
+++ LeetCode/链表/82. 删除排序链表中的重复元素 II.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(next=head)
+        cur = dummy
+        while cur.next and cur.next.next:
+            val = cur.next.val
+            if cur.next.next.val == val:
+                while cur.next and cur.next.val == val:
+                    cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return dummy.next    
